00a12165c7595e0d9705f1a0e8ecbeb9e282e232
confident-collaborator-2097 <seanzngo@gmail.com>
2020-10-29 19:51:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,7 @@ def pop_with_args(array)
   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
    @small_dogs = @dog_breeds.pop(2)
  end
+ 
+ def using_shift(array)
+   using_shift.shift(2)
+ end

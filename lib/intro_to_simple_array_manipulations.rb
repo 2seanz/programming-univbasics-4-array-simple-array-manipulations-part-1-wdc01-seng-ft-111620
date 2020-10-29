043d43a40025cb89be956cdf79ac82b319942cb7
confident-collaborator-2097 <seanzngo@gmail.com>
2020-10-29 19:58:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,4 +20,8 @@ def pop_with_args(array)
   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
    @im_so_over_this_city= @my_favorite_cities.shift
  end
-  
+ 
+ def shift_with_args(array)
+   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  @im_so_over_this_city= @my_favorite_cities.shift(2)
+ end
